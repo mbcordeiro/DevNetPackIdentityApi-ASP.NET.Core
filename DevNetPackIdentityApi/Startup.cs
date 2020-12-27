db932610953d13bf0b5bec0e5598eb02a4724ba3
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-26 21:34:46 -0300

    Swagger config

--- DevNetPackIdentityApi/Startup.cs
+++ DevNetPackIdentityApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using NetDevPack.Identity;
 using NetDevPack.Identity.Jwt;
 
@@ -30,6 +31,18 @@ namespace DevNetPackIdentityApi
             services.AddIdentityEntityFrameworkContextConfiguration(Configuration, GetType().Namespace, "DefaultConnection");
             services.AddJwtConfiguration(Configuration, "AppSettings");
             services.AddIdentityConfiguration();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo() 
+                {
+                    Title = "NetDevPack Identity Sample API",
+                    Description = "Development by Matheus Cordeiro", 
+                    Contact = new OpenApiContact() { Name = "Matheus Cordeiro", Email = "matheusdebarroscordeiro@gmail.cocm"},
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/mit") }
+
+                });
+            });
+
+           
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -38,6 +51,12 @@ namespace DevNetPackIdentityApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
 
             app.UseHttpsRedirection();
 
